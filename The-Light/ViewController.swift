23cb9bcf378005fd24dfb2d5ad9d283fed4f6f1c
Israelyan Leonid <leo.israelyan@icloud.com>
2021-04-09 16:42:39 +0300

    Initial Commit

---
+++ The-Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The-Light
+//
+//  Created by Леонид Исраелян on 09.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
