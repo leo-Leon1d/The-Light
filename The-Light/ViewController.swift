9270c18aae7920a1947c4cf96510e66b2a810b64
Israelyan Leonid <leo.israelyan@icloud.com>
2021-04-15 11:38:39 +0300

    Added colourful background tap switching and a button, which turns your phone camera's LED flashlight on and off also by a tap.

--- The-Light/ViewController.swift
+++ The-Light/ViewController.swift
@@ -6,32 +6,77 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    // MARK: Properties
-
-    func updateUI() {
-        view.backgroundColor = isLightOn ? .white : .black
-    }
     
-var isLightOn = true
+    var light = 0
+    var isTorch = false
+    //MARK: - Properies
     
+    //Hide status bar
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
-    // MARK: Methods
+    //MARK: - Methods
     
-    override func  viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidLoad() {
         updateUI()
+    }
+    //Screen color update function
+    fileprivate func updateUI() {
+        switch light{
+        case 0:
+            view.backgroundColor = .red
+            light = 1
+        case 1:
+            view.backgroundColor = .yellow
+            light = 2
+        case 2:
+            view.backgroundColor = .green
+            light = 0
+        default:
+            print(" ")
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLightOn.toggle()
         updateUI()
     }
     
-
+    //Turning on and off camera flash light function
+    @IBAction func buttonPressed(_ sender: Any) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else {
+            return
+        }
+        
+        if device.hasTorch {
+            isTorch = !isTorch
+            
+            do {
+                try device.lockForConfiguration()
+                
+                if isTorch == true {
+                    device.torchMode = .on
+                }
+                
+                else {
+                    device.torchMode = .off
+                    
+                }
+                device.unlockForConfiguration()
+                
+                
+            }
+            catch {
+                print("Torch is not working")
+            }
+            
+        }
+        else {
+            print("Torch is not compatible with device")
+        }
+    }
 }
+
